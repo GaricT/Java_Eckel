1b86a314bb84e51d3a280d8e4442cae820a538ba
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2017-11-12 21:58:19 +0300

    Опечатка

--- src/com/garict/HelloDate.java
+++ src/com/garict/HelloDate.java
@@ -1,9 +1,9 @@
-// package com.garict;
+package com.garict;
 import java.util.*;
 
 public class HelloDate {
     public static void main (String[] args) {
-        System.out.println("Привет, сегодная: ");
+        System.out.println("Привет, сегодня: ");
         System.out.println(new Date());
     }
 }
