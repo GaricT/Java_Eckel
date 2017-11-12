c4cb686b2aeec721c9ce15f0c9981f7b3ddc6113
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2017-11-12 22:24:49 +0300

    Упражнения 1 глава 2.

--- src/com/garict/less_ch02.java
+++ src/com/garict/less_ch02.java
@@ -4,6 +4,9 @@ import java.util.*;
 class tstclass01 {
     int fI;
     String fTxt;
+    int storage (String s){
+        return s.length() * 2;
+    }
 }
 
 public class less_ch02 {
@@ -12,6 +15,7 @@ public class less_ch02 {
         vTstCl.fTxt = "New data";
         System.out.println("fI = " + vTstCl.fI);
         System.out.println("fTxt = " + vTstCl.fTxt);
+        System.out.println("storage() = " + vTstCl.storage(vTstCl.fTxt));
 
     }
 
