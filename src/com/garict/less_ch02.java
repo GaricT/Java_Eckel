891c430bd4fef89073031bd937763e58a6220585
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2017-11-12 22:20:09 +0300

    Упражнения 1 глава 2.

--- src/com/garict/less_ch02.java
+++ src/com/garict/less_ch02.java
@@ -9,6 +9,7 @@ class tstclass01 {
 public class less_ch02 {
     public static void main (String[] args){
         tstclass01 vTstCl = new tstclass01();
+        vTstCl.fTxt = "New data";
         System.out.println("fI = " + vTstCl.fI);
         System.out.println("fTxt = " + vTstCl.fTxt);
 
