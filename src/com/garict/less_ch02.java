4c6ab7df0082a04742b64a304ae55162603bc260
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2017-11-14 22:55:23 +0300

    Упражнение 8 глава 2.

--- src/com/garict/less_ch02.java
+++ src/com/garict/less_ch02.java
@@ -23,8 +23,20 @@ public class less_ch02 {
         System.out.println(StaticTest.i);
 */
 // Упражнение 8
+        // Порождаем новые объект класса StaticTest
         StaticTest StatCls = new StaticTest();
-
+        // Присваиваем статическому полю новое значени, теперь 49 будет как в StatCls.i так и в StaticTest.i
+        StatCls.i = 49;
+        // Вызываем инкремент для значения StaticTest.i, теперь там 50
+        Incrementable.increment();
+        // Порождаем еще один объект класса
+        StaticTest StatCls02 = new StaticTest();
+        // Творим сумашествие: В новый объект записываем сумму значений из объекта класса и самого класса
+        StatCls02.i = StatCls.i + StaticTest.i;
+        // Убеждаемся, что везде одинаковое значение
+        System.out.println("StaticTest.i = " + StaticTest.i);
+        System.out.println("StatCls.i    = " + StatCls.i);
+        System.out.println("StatCls02.i  = " + StatCls02.i);
 //
     }
 
