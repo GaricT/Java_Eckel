ae25d15390a8c9c86662c49950b6899b79b9d393
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2017-11-14 22:43:43 +0300

    Упражнения 7 глава 2.

--- src/com/garict/less_ch02.java
+++ src/com/garict/less_ch02.java
@@ -11,12 +11,30 @@ class tstclass01 {
 
 public class less_ch02 {
     public static void main (String[] args){
-        tstclass01 vTstCl = new tstclass01();
-        vTstCl.fTxt = "New data";
-        System.out.println("fI = " + vTstCl.fI);
-        System.out.println("fTxt = " + vTstCl.fTxt);
-        System.out.println("storage() = " + vTstCl.storage(vTstCl.fTxt));
+//        tstclass01 vTstCl = new tstclass01();
+//        vTstCl.fTxt = "New data";
+//        System.out.println("fI = " + vTstCl.fI);
+//        System.out.println("fTxt = " + vTstCl.fTxt);
+//        System.out.println("storage() = " + vTstCl.storage(vTstCl.fTxt));
 
+// Упраженение 7
+/*
+        Incrementable.increment();
+        System.out.println(StaticTest.i);
+*/
+// Упражнение 8
+        StaticTest StatCls = new StaticTest();
+
+//
     }
 
+}
+
+// Упражнение 7
+class StaticTest {
+    static int i = 47;
+}
+
+class Incrementable {
+    static void increment () {StaticTest.i++;}
 }
