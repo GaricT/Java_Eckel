f1befcb18c8182d10151cbac4b518cbc59a4cee0
garic-r@yandex.ru <n88jT6xsEheTGTPBLH9WO6T2d8>
2017-11-16 23:17:09 +0300

    Упражнение 2. Глава 3.

--- src/com/garict/less_ch03.java
+++ src/com/garict/less_ch03.java
@@ -1,4 +1,28 @@
 package com.garict;
 
+class LessOne{
+    float fFloat;
+}
+
 public class less_ch03 {
+    public static void main(String[] args){
+        // Упражнение 2
+        LessOne objLOne01 = new LessOne();
+        LessOne objLOne02 = new LessOne();
+        // Инициализация
+        objLOne01.fFloat = 3.21f;
+        objLOne02.fFloat = 4.56f;
+        System.out.println("1: " + objLOne01.fFloat + " : " + objLOne02.fFloat);
+        // Присваиваем
+        objLOne01 = objLOne02;
+        System.out.println("2: " + objLOne01.fFloat + " : " + objLOne02.fFloat);
+        // Изменяем поле в 1 классе
+        objLOne01.fFloat = 77.11f;
+        System.out.println("3: " + objLOne01.fFloat + " : " + objLOne02.fFloat);
+        // Изменяем поле во 2 классе
+        objLOne02.fFloat = 88.22f;
+        System.out.println("3: " + objLOne01.fFloat + " : " + objLOne02.fFloat);
+
+    }
+
 }
